fe2609155c9c021404347fc4da6057349b66c5af
Veronika050987 <veronicaviv@mail.ru>
2026-06-29 21:52:49 +0300

    functions with arrays HW done

--- index.php
+++ index.php
@@ -137,7 +137,7 @@ function dump_wrapper($value)
 			</li>
 		</ol>
 	</details>
-	<details open>
+	<details>
 		<summary>Массивы</summary>
 		<h3>Индексированные массивы:</h3>
 		<?php
@@ -224,6 +224,290 @@ function dump_wrapper($value)
 		print_r($multi);
 		echo '</pre>';
 		?>
+	</details>
+	<details open>
+		<summary>PHP array functions</summary>
+		<pre>
+		<?php
+
+echo "<h2>Count an array\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		echo count($cars);
+		echo '<hr>';
+
+echo "<h2>Create and display an indexed array 1\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		var_dump($cars);
+		echo '<hr>';
+
+echo "<h2>Create and display an indexed array 2\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		print_r($cars);
+		echo '<hr>';
+
+echo "<h2>Create and display an indexed array 3\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		dump_wrapper($cars);
+		echo '<hr>';
+
+echo "<h2>Access an array item\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		echo $cars[1];
+		echo '<hr>';
+
+echo "<h2>Access an accosiative array item\n</h2>";
+		$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+		echo $cars["year"];
+		echo '<hr>';
+
+echo "<h2>Delete an array element\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		unset($cars[2]);
+		dump_wrapper($cars);
+		echo '<hr>';
+
+echo "<h2>Add an array element\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		$cars[] = 'Haval';
+		dump_wrapper($cars);
+		echo '<hr>';
+
+echo "<h2>Change an array element\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		dump_wrapper($cars);
+		$cars[1] = 'Ford';
+		dump_wrapper($cars);
+		echo '<hr>';
+
+echo "<h2>Declare an empty array\n</h2>";
+		$cars = [];
+		$cars[0] = "Volvo";
+		$cars[1] = "BMW";
+		$cars[2] = "Toyota";
+		dump_wrapper($cars);
+		echo '<hr>';
+
+echo "<h2>Declare an empty associative array\n</h2>";
+		$myCar = [];
+		$myCar["brand"] = "Ford";
+		$myCar["model"] = "Mustang";
+		$myCar["year"] = 1964;
+		dump_wrapper($myCar);
+		echo '<hr>';
+
+echo "<h2>Mixing array keys\n</h2>";
+		$myArr = [];
+		$myArr[0] = "apples";
+		$myArr[1] = "bananas";
+		$myArr["fruit"] = "cherries";
+		dump_wrapper($myArr);
+		echo '<hr>';
+
+echo "<h2>Execute a function item\n</h2>";
+		function myFunction() {
+		echo "I come from a function!";
+		}
+
+		$myArr = array("Volvo", 15, "myFunction");
+
+		$myArr[2]();
+		echo '<hr>';
+		function myFunctions() {
+		echo "I come from a fun function!";
+		}
+
+		$myArr = array("car" => "Volvo", "age" => 15, "message" => "myFunctions");
+
+		$myArr["message"]();
+		echo '<hr>';
+
+echo "<h2>Update array items in a foreach loop\n</h2>";
+		$cars = array("Volvo", "Citroen", "Audi");
+		var_dump($cars);
+		echo '<hr>';
+		foreach ($cars as &$x) {
+		$x = "Ford";
+		}
+		unset($x);
+		var_dump($cars);
+		echo '<hr>';
+
+echo "<h2>Add a single array item\n</h2>";
+		$fruits = array("Apple", "Banana", "Cherry");
+		var_dump($fruits);
+		$fruits[] = "Orange";
+		var_dump($fruits);
+		echo '<hr>';
+
+echo "<h2>Add two or more array items\n</h2>";
+		$fruits = array("Apple", "Banana", "Cherry");
+		var_dump($fruits);
+		$fruits[] = "Orange";
+		$fruits[] = "Pears";
+		var_dump($fruits);
+		echo '<hr>';
+
+echo "<h2>Add into an associative array\n</h2>";
+		$cars = array("brand" => "Ford", "model" => "Mustang");
+		var_dump($cars);
+		$cars["color"] = "Red";
+		var_dump($cars);
+		echo '<hr>';
+
+echo "<h2>Add into array</h2>";
+//In PHP, you can add array items with several different methods:
+
+//[] - adds a single item to the end of an array
+//array_push() - adds one or more items to the end of an array
+//array_unshift() - adds one or more items to the beginning of an array
+//array_splice() - removes a portion of an array and replaces it with new elements
+//array_merge() - merges two or more arrays
+echo "<h2>array_push() function \n</h2>"; //The array_push() function is used to add one or more array items to the end of an existing array.
+		$fruits = array("Apple", "Banana", "Cherry");
+		var_dump($fruits);
+		array_push($fruits, "Orange", "Kiwi", "Lemon");
+		var_dump($fruits);
+		echo '<hr>';
+
+echo "<h2>Add mupltiple items to an associative array\n</h2>";
+		$cars = array("brand" => "Ford", "model" => "Mustang");
+		var_dump($cars);
+		$cars += ["color" => "red", "year" => 1964];
+		var_dump($cars);
+		echo '<hr>';
+
+echo "<h2>array_unshift() Function\n</h2>";//The array_unshift() function is used to add one or more array items to the beginning of an existing array
+		$fruits = array("Apple", "Banana", "Cherry");
+		var_dump($fruits);
+		array_unshift($fruits, "Orange", "Kiwi", "Lemon");
+		var_dump($fruits);
+		echo '<hr>';
+
+echo "<h2>array_splice() Function\n</h2>";//The array_splice() function is used remove a portion of an array and replace it with new items. If you specify an offset and a length of 0 (nothing to remove), you can insert an item at that position.
+		$fruits = array("Apple", "Banana", "Cherry");
+		var_dump($fruits);
+		$new_fruit = "Orange";
+		array_splice($fruits, 1, 0, $new_fruit);
+		var_dump($fruits);
+		echo '<hr>';
+
+echo "<h2>array_merge() Function</h2>";//The array_merge() function is used to merge two or more arrays.
+		$fruits1 = array("Apple", "Banana");
+		var_dump($fruits1);
+		$fruits2 = array("Cherry", "Orange");
+		var_dump($fruits2);
+		$result = array_merge($fruits1, $fruits2);
+		var_dump($result);
+		echo '<hr>';
+
+echo "<h2>Delete array items functions</h2>";
+//In PHP, you can remove/delete array items with several different functions:
+
+//array_splice() - removes a portion of the array starting from a start position and length
+//unset() - removes the element associated with a specific key
+//array_diff() - remove items from an associative array
+//array_pop() - removes the last array item
+//array_shift() - removes the first array item
+
+echo "<h2>array_splice() Function</h2>";//With the array_splice() function you specify the index (where to start) and how many items you want to delete. After the deletion, the array gets re-indexed automatically, starting at index 0.
+		$cars = array("Volvo", "BMW", "Toyota");
+		var_dump($cars);
+		array_splice($cars, 1, 1);
+		var_dump($cars);
+		echo '<hr>';
+echo "<h2>array_splice() Function for mupltiple elements</h2>";//With the array_splice() function you specify the index (where to start) and how many items you want to delete. After the deletion, the array gets re-indexed automatically, starting at index 0.
+		$cars = array("Volvo", "BMW", "Toyota");
+		var_dump($cars);
+		array_splice($cars, 1, 2);
+		var_dump($cars);
+		echo '<hr>';
+echo "<h2>unset() Function</h2>";//Note: The unset() function does not re-index the array. So, if you remove an element at index 1, the other elements (e.g., at index 0, 2, 3, etc.) will keep their original indices, leading to a "gap" in the sequence of indices.
+		$cars = array("Volvo", "BMW", "Toyota");
+		var_dump($cars);
+		unset($cars[1]);
+		var_dump($cars);
+		echo '<hr>';
+echo "<h2>unset() Function for mupltiple elements</h2>";//Note: The unset() function does not re-index the array. So, if you remove an element at index 1, the other elements (e.g., at index 0, 2, 3, etc.) will keep their original indices, leading to a "gap" in the sequence of indices.
+		$cars = array("Volvo", "BMW", "Toyota");
+		var_dump($cars);
+		unset($cars[0], $cars[2]);
+		var_dump($cars);
+		echo '<hr>';
+echo "<h2>array_diff() Function</h2>";//You can also use the array_diff() function to remove items from an associative array. This function returns a new array, without the specified items.
+		$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+		var_dump($cars);
+		$newarray = array_diff($cars, ["Mustang", 1964]);
+		var_dump($newarray);
+		echo '<hr>';
+echo "<h2>array_pop() Function</h2>";//The array_pop() function removes the last item of an array.
+		$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+		var_dump($cars);
+		array_pop($cars);
+		var_dump($cars);
+		echo '<hr>';
+echo "<h2>array_shift() Function</h2>";//The array_shift() function removes the first item of an array.
+		$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+		var_dump($cars);
+		array_shift($cars);
+		var_dump($cars);
+		echo '<hr>';
+echo"<h2>Sorting arrays</h2>";
+//Here are the main PHP array sorting functions:
+
+//sort() - sorts an indexed array in ascending order
+//rsort() - sorts an indexed array in descending order
+//asort() - sorts an associative array in ascending order, according to the value
+//ksort() - sorts an associative array in ascending order, according to the key
+//arsort() - sorts an associative array in descending order, according to the value
+//krsort() - sorts an associative array in descending order, according to the key
+echo "<h2>sort() Function</h2>";//The sort() function sorts an indexed array in ascending order.
+		$cars = array("Volvo", "BMW", "Toyota");
+		var_dump($cars);
+		sort($cars);
+		var_dump($cars);
+		echo '<hr>';
+		$numbers = array(4, 6, 2, 22, 11);
+		print_r($numbers);
+		sort($numbers);
+		print_r($numbers);
+		echo '<hr>';
+echo "<h2>rsort() Function</h2>";//The rsort() function sorts an indexed array in descending order.
+		$cars = array("Volvo", "BMW", "Toyota");
+		var_dump($cars);
+		rsort($cars);
+		var_dump($cars);
+		echo '<hr>';
+		$numbers = array(4, 6, 2, 22, 11);
+		print_r($numbers);
+		rsort($numbers);
+		print_r($numbers);
+		echo '<hr>';
+echo "<h2>asort() Function</h2>";//The asort() function sorts an associative array in ascending order, according to the value.
+		$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+		var_dump($age);
+		asort($cars);
+		var_dump($age);
+		echo '<hr>';
+echo "<h2>arsort() Function</h2>";//The arsort() function sorts an associative array in descending order, according to the value.
+		$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+		var_dump($age);
+		arsort($age);
+		var_dump($age);
+		echo '<hr>';
+echo "<h2>ksort() Function</h2>";//The ksort() function sorts an associative array in ascending order, according to the key.
+		$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+		print_r($age);
+		ksort($cars);
+		print_r($age);
+		echo '<hr>';
+echo "<h2>krsort() Function</h2>";//The krsort() function sorts an associative array in descending order, according to the key.
+		$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+		print_r($age);
+		krsort($age);
+		print_r($age);
+		echo '<hr>';
+		?>
+		</pre>
 	</details>
 </body>
 </html>
