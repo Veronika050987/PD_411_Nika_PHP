9d324ba07c5f15f64c7fa00ce291b3d5d54f0f2f
Veronika050987 <veronicaviv@mail.ru>
2026-07-01 19:51:45 +0300

    project organisation classwork

---
+++ 02-organisation/main.php
@@ -0,0 +1,10 @@
+<?php include_once 'inc/header.php'; ?>
+<?php //require 'inc/header.php'; ?>
+<?php include_once 'data.php'; ?>
+    <h1><?= $title; ?></h1>
+    <h2><?= $message; ?></h2>
+    <h2>MAGIC CONSTANTS:</h2>
+    <h3>__DIR__: <?= __DIR__?></h3>
+    <h3>__FILE__: <?= __FILE__ ?></h3>
+    <h4><?= Message();?></h4>
+<?php include_once 'inc/footer.php'; ?>
