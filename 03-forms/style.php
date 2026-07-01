061b35512650d1a78edde02083f6440c8d489c17
Veronika050987 <veronicaviv@mail.ru>
2026-07-01 20:06:02 +0300

    style.css repaired

--- 03-forms/style.php
+++ 03-forms/style.php
@@ -5,13 +5,12 @@ header("Content-type:text/css; charset=UTF-8");
 $first_color = "lightskyblue";
 $second_color = "pink";
 
-echo __FILE__;
-
 ?>
 
 body
 {
-	background-image: <?php echo "linear-gradient(to bottom right, $first_color, $second_color);"; ?>
+	background-image: linear-gradient(to bottom right, <?php echo $first_color; ?>, <?php echo $second_color; ?>);
+	background-attachment: fixed;
 }
 form
 {
