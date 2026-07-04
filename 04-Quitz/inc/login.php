ea3cae6c772e53adb258c4e79ae73170419c6fc8
Veronika050987 <veronicaviv@mail.ru>
2026-07-04 12:33:26 +0300

    colors changed for login form and button submit made (205 px)

--- 04-Quitz/inc/login.php
+++ 04-Quitz/inc/login.php
@@ -4,5 +4,5 @@
 	<div class="login-item"><label for="first_name">	Имя:	 </label><input type="text" name="first_name"	id="first_name"		placeholder="Имя"></div>
 	<div class="login-item"><label for="middle_name">	Отчество:</label><input type="text" name="middle_name"	id="middle_name"	placeholder="Отчество"></div>
 	<div class="login-item"><label for="email">			Почта:	 </label><input type="text" name="email"		id="email"			placeholder="E-mail"></div>
-	<div class="login-item"><label style="width:208px;"></label><button type="submit">Start</button></div>
+	<div class="login-item"><label style="width:204px;"></label><button type="submit">Start</button></div>
 </form>
