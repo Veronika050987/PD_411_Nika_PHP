d11d0fe28e7d272ac540a7e9849068d3321119ed
Veronika050987 <veronicaviv@mail.ru>
2026-07-23 20:41:28 +0300

    Nika storage in file DONE

--- 07-OOP/inc/main.php
+++ 07-OOP/inc/main.php
@@ -85,4 +85,81 @@ foreach ($list as $item) {
 echo "<pre>";
 print_r($grouped);
 echo "</pre>";
+
+///////////with file saving
+$list = [
+    [
+        "last_name" => "Pinkman",
+        "first_name" => "Jessie",
+        "age" => 20,
+        "spec" => "Chemistry",
+        "group" => "WW_220",
+        "rating" => 90,
+        "attendance" => 95
+    ],
+    [
+        "last_name" => "Vercetty",
+        "first_name" => "Tommy",
+        "age" => 30,
+        "spec" => "Criminalistic",
+        "group" => "WW_225",
+        "rating" => 93,
+        "attendance" => 90
+    ],
+    [
+        "last_name" => "Rosenberg",
+        "first_name" => "Ken",
+        "age" => 29,
+        "spec" => "Chemistry",
+        "group" => "WW_220",
+        "rating" => 57,
+        "attendance" => 100
+    ],
+    [
+        "last_name" => "Carrington",
+        "first_name" => "Avery",
+        "age" => 32,
+        "spec" => "Criminalistic",
+        "group" => "WW_225",
+        "rating" => 63,
+        "attendance" => 78
+    ],
+    [
+        "last_name" => "Cassidy",
+        "first_name" => "Phil",
+        "age" => 23,
+        "spec" => "Chemistry",
+        "group" => "WW_220",
+        "rating" => 63,
+        "attendance" => 78
+    ]
+];
+function array_group_by_multi(array $array, string ...$keys): array {
+    $result = [];
+    foreach ($array as $item) {
+        $current = &$result;
+        foreach ($keys as $key) {
+            $value = $item[$key] ?? null;
+            $current = &$current[$value];
+        }
+        $current[] = $item;
+    }
+    return $result;
+}
+$grouped = array_group_by_multi($list, "group");
+
+$textData = print_r($grouped, true);
+
+$fileName = "grouped_students.txt";
+
+if (file_put_contents($fileName, $textData) !== false) //function for file storage
+{
+    echo "Data saved in file: " . $fileName;
+} else
+    "Error during file saving...";
+
+echo "<pre>";
+print_r($grouped);
+echo "</pre>";
+
 ?>
